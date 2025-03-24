342aff99c7d498f498c075435b9770554f1aa52d
Ombongi <fombongi11@gmail.com>
2025-03-24 11:38:11 +0300

    adjust the map.urls

--- TrafficMS/TrafficMS/TrafficMS/settings.py
+++ TrafficMS/TrafficMS/TrafficMS/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'TrafficMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'',
-        'USER':'',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
--- TrafficMS/TrafficMS/map/urls.py
+++ TrafficMS/TrafficMS/map/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home_view, name='home_view'),  # Root URL path
-    path('base', views.base_view, name='base_view'),  # Base URL path
+    path('home', views.home_view, name='home'),  # Root URL path
+    path('base', views.base_view, name='base'),  # Base URL path
     # Map views
-    path('map/', views.map_view, name='map_view'),
+    path('map/', views.map_view, name='map'),
     path('api/map-data/', views.get_map_data, name='get_map_data'),
     
     # Routing views
-    path('reroute/', views.reroute_view, name='reroute_view'),
+    path('reroute/', views.reroute_view, name='reroute'),
     path('api/routes-data/', views.get_routes_data, name='get_routes_data'),
 ]
