e6bbcced7fbb6d740d64b5ef81407c90174bb09f
konzolaw <125367223+konzolaw@users.noreply.github.com>
2025-04-08 09:40:14 +0300

    added changes to .yml

--- smarttraffic-aiwebsite/next.config.ts
+++ smarttraffic-aiwebsite/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
@@ -10,3 +14,7 @@ export default nextConfig;
 module.exports = {
   output: "export",
 };
+
+
+
+
